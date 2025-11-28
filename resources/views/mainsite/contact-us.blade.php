e41fd7aa58c9fd4ebce06260a608936b650fa0c3
Unyc1124 <aditidubey792@gmail.com>
2025-11-28 23:34:50 +0530

    ok test aditi

--- resources/views/mainsite/contact-us.blade.php
+++ resources/views/mainsite/contact-us.blade.php
@@ -75,7 +75,7 @@
                                 <h3>Our Address</h3>
                                 <div class="listtype">
                                     <a>
-                                        Mind Rewire, Park Ivory, Park Street, Wakad, Pune, Maharashtra, India -
+                                        Aditi Dubey, Park Ivory, Park Street, Wakad, Pune, Maharashtra, India -
                                         421501
                                     </a>
                                 </div>
