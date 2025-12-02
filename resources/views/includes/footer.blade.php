8cc07bc2ee2a9dc722d8216cb32a9b4bc47a9a24
Anand Singh <92910742+Gttpoc@users.noreply.github.com>
2025-12-02 13:46:26 +0530

    Anand Linkedin

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -178,7 +178,7 @@
                     </div>
                     <div class="managed_by">
                         <p>
-                            ✨ Built by Anand with ❤️
+                            ✨ Built by <a href="https://www.linkedin.com/in/anand-singh-286a0a50/">Anand</a> with ❤️
                         </p>
                     </div>
                 </div>
